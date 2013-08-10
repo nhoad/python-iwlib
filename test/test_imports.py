29b9833d81c6b162093b5642a71cbd6d6320d60f
Nathan Hoad <nathan@getoffmalawn.com>
2013-08-10 10:06:36 +1000

    A simple test to make sure I don't break setup.py horribly again.

---
+++ test/test_imports.py
@@ -0,0 +1,19 @@
+def test_imports_dont_fail():
+    '''Gross test for ensuring packaging hasn't been broken.'''
+    imports = [
+        "iwlib.iwconfig.get_iwconfig",
+        "iwlib.iwlist.scan",
+        "iwlib.utils.get_max_quality",
+        "iwlib.utils.set_essid",
+        "iwlib.utils.supports_scanning",
+    ]
+
+    for imp in imports:
+        module, _from = imp.rsplit('.', 1)
+        print 'from %s import %s' % (module, _from)
+        __import__(module, fromlist=[_from])
+
+
+def test_backwards_compatibility():
+    '''Ensure backwards compatibility with the module before I came along.'''
+    from iwlib import get_iwconfig
